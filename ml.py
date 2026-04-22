ad97601fe34c22385efdc3448ddfbb87c42d43bf
Harshit Adhikari <adhikariharry399@gmail.com>
2026-04-22 13:15:05 +0530

    Update ml.py

--- ml.py
+++ ml.py
@@ -4,57 +4,50 @@ import seaborn as sns
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score, confusion_matrix, accuracy_score
+
 
 df = pd.read_csv(r"C:\Users\harsh\OneDrive\Desktop\cs2 python\loan_approval_dataset.csv")
 
-
 df.columns = df.columns.str.strip()
-
-print("First 5 rows:")
-print(df.head())
-
-print("\nDataset Shape:", df.shape)
 
 
 df.drop_duplicates(inplace=True)
 
-
 le = LabelEncoder()
-
 for col in ['education', 'self_employed', 'loan_status']:
     df[col] = le.fit_transform(df[col].astype(str).str.strip())
 
 
-X = df.drop(['loan_id', 'loan_status'], axis=1)
-y = df['loan_status']
-
-
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
+X = df[['income_annum']]   
+y = df['loan_status']      
 
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X_scaled, y, test_size=0.2, random_state=42
+    X, y, test_size=0.2, random_state=42
 )
 
 
-model = LogisticRegression(max_iter=1000)
+model = LinearRegression()
 model.fit(X_train, y_train)
 
 
 y_pred = model.predict(X_test)
 
 
+y_pred_binary = [1 if i > 0.5 else 0 for i in y_pred]
+
+
 print("\n===== MODEL PERFORMANCE =====")
-print("Accuracy Score:", accuracy_score(y_test, y_pred))
 
-print("\nClassification Report:")
-print(classification_report(y_test, y_pred))
+print("R2 Score:", r2_score(y_test, y_pred))
+print("Mean Squared Error:", mean_squared_error(y_test, y_pred))
+
+print("Accuracy (after threshold):", accuracy_score(y_test, y_pred_binary))
 
 
-cm = confusion_matrix(y_test, y_pred)
+cm = confusion_matrix(y_test, y_pred_binary)
 
 print("\nConfusion Matrix:")
 print(cm)
